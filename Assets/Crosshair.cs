bf9ce7248cfe9b605d76a2dcf0b71a0ced2b891d
Chris Wagner <Chris Wagner>
2017-06-07 11:00:47 +0200

    Added Crosshair

---
+++ Assets/Crosshair.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        float length = 100;
+        float thickness = 0.1f;
+        GUI.Box(new Rect(Screen.width / 2 - length/2, Screen.height / 2 - thickness/2, length, thickness), "");
+        GUI.Box(new Rect(Screen.width / 2 - thickness/2, Screen.height / 2 - length/2, thickness, length), "");
+    }
+}
