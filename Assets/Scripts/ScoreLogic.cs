e417fdf4bf265fdc02651b84c1b4533f617a5746
Chris Wagner <Chris Wagner>
2017-06-14 14:40:42 +0200

    Added countdown timer

--- Assets/Scripts/ScoreLogic.cs
+++ Assets/Scripts/ScoreLogic.cs
@@ -1,27 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreLogic : MonoBehaviour {
 
     public static int Score = 0;
+    private static float mTimeLeft = 60;
+    public static float TimeLeft
+    {
+        get { return mTimeLeft; }
+    }
+
+    private void Start()
+    {
+        mTimeLeft = 60;
+    }
 
     void OnGUI()
     {
         float x = Screen.width * 0.9f - Screen.height * 0.02f;
         float y = Screen.height * 0.05f;
-        var scoreStyle = new GUIStyle();
-        scoreStyle.fontSize = 50;
-        scoreStyle.alignment = TextAnchor.UpperCenter;
-        scoreStyle.fontStyle = FontStyle.Bold;
-        scoreStyle.normal.textColor = Color.white;
-        scoreStyle.alignment = TextAnchor.MiddleRight;
+        var textStyle = new GUIStyle();
+        textStyle.fontSize = 50;
+        textStyle.alignment = TextAnchor.UpperCenter;
+        textStyle.fontStyle = FontStyle.Bold;
+        textStyle.normal.textColor = Color.white;
+        textStyle.alignment = TextAnchor.MiddleRight;
+        GUI.Label(new Rect(x, y, Screen.width * 0.1f, Screen.height * 0.05f), Score.ToString(), textStyle);
 
-        GUI.Label(new Rect(x, y, Screen.width * 0.1f, Screen.height * 0.05f), Score.ToString(), scoreStyle);
+        x = Screen.width * 0.9f - Screen.height * 0.02f;
+        y = Screen.height * 0.9f;
+        
+        GUI.Label(new Rect(x, y, Screen.width * 0.1f, Screen.height * 0.05f), Mathf.Max(0, Mathf.Floor(mTimeLeft)).ToString(), textStyle);
     }
 
     // Update is called once per frame
     void Update () {
-		
+        mTimeLeft -= Time.deltaTime;
+        if (mTimeLeft <= 0)
+        {
+            //TODO Finish
+            SceneManager.LoadScene("StartMenu");
+        }
 	}
 }
