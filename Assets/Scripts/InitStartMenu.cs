90769bb3dd642ce600439fd3350d58da19f2118a
Koelli91 <kevinkoellmann@outlook.com>
2017-06-21 12:15:01 +0200

    Added some Scripts

---
+++ Assets/Scripts/InitStartMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InitStartMenu : MonoBehaviour
+{
+	void Start()
+    {
+        ScoreLogic.Init();
+
+        GameObject lastScoreObj = GameObject.FindWithTag("LastScore");
+        GameObject highScoreObj = GameObject.FindWithTag("HighScore");
+
+        if (null != lastScoreObj)
+        {
+            Text lastScoreText = lastScoreObj.GetComponent<Text>();
+            if (null != lastScoreText)
+            {
+                lastScoreText.text = ScoreLogic.LastScore.ToString();
+            }
+        }
+
+        if (null != highScoreObj)
+        {
+            Text highScoreText = highScoreObj.GetComponent<Text>();
+            if (null != highScoreText)
+            {
+                highScoreText.text = ScoreLogic.HighScore.ToString();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TimeLogic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeLogic : MonoBehaviour
+{
+    [Tooltip("Round time in seconds")]
+    public int RoundTime = 60;
+    private static float timeLeft;
+
+    public static float TimeLeft
+    {
+        get
+        {
+            return Mathf.Max(0, Mathf.Floor(timeLeft));
+        }
+    }
+
+    void Start ()
+    {
+        timeLeft = RoundTime;
+    }
+	
+	void Update ()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (TimeLeft <= 0)
+        {
+            SceneManager.LoadScene("StartMenu");
+        }
+    }
+}
