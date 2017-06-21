e0b14f6e63c4e6178b7b38e3551e9b52fb0896a5
Chris Wagner <Chris Wagner>
2017-06-21 09:36:09 +0200

    Move along waypoints

--- Assets/Scripts/Waypoints.cs
+++ Assets/Scripts/Waypoints.cs
@@ -3,12 +3,54 @@
 public class Waypoints : MonoBehaviour {
     public static Transform[] waypoints;
 
-    void Awake()
+    // put the points from unity interface
+    public Transform[] wayPointList;
+
+    public int currentWayPoint = 0;
+    Transform targetWayPoint;
+
+    public float speed = 4f;
+
+    // Use this for initialization
+    void Start()
     {
-        waypoints = new Transform[transform.childCount];
-        for (int i=0; i<waypoints.Length; i++)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if we have somewere to walk
+        if (currentWayPoint < this.wayPointList.Length)
         {
-            waypoints[i] = transform.GetChild(i);
+            if (targetWayPoint == null)
+                targetWayPoint = wayPointList[currentWayPoint];
+            move();
+        }
+    }
+
+    void move()
+    {
+        // rotate towards the target
+        
+        // Schiffboden zur Erdegerichtet
+
+
+        // move towards the target
+        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+
+        if (transform.position == targetWayPoint.position)
+        {
+            currentWayPoint++;
+            if (currentWayPoint < this.wayPointList.Length)
+            {
+                targetWayPoint = wayPointList[currentWayPoint];
+            }
+            else
+            {
+                currentWayPoint = 0;
+                targetWayPoint = wayPointList[currentWayPoint];
+            }
         }
     }
 }
